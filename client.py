a13f9bc2b09e737d675eead0aee42c70aeb44acd
MikhailKardash <48943383+MikhailKardash@users.noreply.github.com>
2021-03-15 12:44:46 -0700

    Update server
    Made server host to localhost:8080 and provide a VideoPlayer

--- client.py
+++ client.py
@@ -9,10 +9,59 @@ from aiortc import (
     VideoStreamTrack,
 )
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
-from aiortc.contrib.signaling import BYE, add_signaling_arguments, create_signaling
+from aiortc.contrib.signaling import BYE, add_signaling_arguments, TcpSocketSignaling
 
+def channel_send(channel, message):
+    channel_log(channel, ">", message)
+    channel.send(message)
 
+async def consume_signaling(pc, signaling):
+    while True:
+        obj = await signaling.receive()
 
+        if isinstance(obj, RTCSessionDescription):
+            await pc.setRemoteDescription(obj)
+
+            if obj.type == "offer":
+                # send answer
+                await pc.setLocalDescription(await pc.createAnswer())
+                await signaling.send(pc.localDescription)
+        elif isinstance(obj, RTCIceCandidate):
+            await pc.addIceCandidate(obj)
+        elif obj is BYE:
+            print("Exiting")
+            break
+
+async def run_offer(pc, signaling):
+    await signaling.connect()
+
+    channel = pc.createDataChannel("chat")
+    channel_log(channel, "-", "created by local party")
+
+    async def send_pings():
+        while True:
+            channel_send(channel, "ping %d" % current_stamp())
+            await asyncio.sleep(1)
+
+    @channel.on("open")
+    def on_open():
+        asyncio.ensure_future(send_pings())
+
+    @channel.on("message")
+    def on_message(message):
+        channel_log(channel, "<", message)
+
+        if isinstance(message, str) and message.startswith("pong"):
+            elapsed_ms = (current_stamp() - int(message[5:])) / 1000
+            print(" RTT %.2f ms" % elapsed_ms)
+
+    # send offer
+    await pc.setLocalDescription(await pc.createOffer())
+    await signaling.send(pc.localDescription)
+
+    await consume_signaling(pc, signaling)
+    
+    
 
 
 
@@ -20,7 +69,7 @@ if __name__ == '__main__':
     print("Starting Server")
     
     # create signaling object
-    signaling = create_signaling()
+    signaling = TcpSocketSignaling()
     
     
     # create peer connection
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import numpy as np
-
+import asyncio
 from av import VideoFrame
+import cv2
 
 from aiortc import (
     RTCIceCandidate,
@@ -9,9 +10,127 @@ from aiortc import (
     VideoStreamTrack,
 )
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
-from aiortc.contrib.signaling import BYE, add_signaling_arguments, create_signaling
+from aiortc.contrib.signaling import BYE, add_signaling_arguments, TcpSocketSignaling
+def channel_send(channel, message):
+    channel_log(channel, ">", message)
+    channel.send(message)
+    
+    
+    
+async def run_answer(pc, signaling):
+    await signaling.connect()
+
+    @pc.on("datachannel")
+    def on_datachannel(channel):
+        channel_log(channel, "-", "created by remote party")
+
+        @channel.on("message")
+        def on_message(message):
+            channel_log(channel, "<", message)
+
+            if isinstance(message, str) and message.startswith("ping"):
+                # reply
+                channel_send(channel, "pong" + message[4:])
+
+    await consume_signaling(pc, signaling)
+    
+    
+class FlagVideoStreamTrack(VideoStreamTrack):
+    """
+    A video track that returns an animated flag.
+    """
+
+    def __init__(self):
+        super().__init__()  # don't forget this!
+        self.counter = 0
+        height, width = 480, 640
+
+        # generate flag
+        data_bgr = np.hstack(
+            [
+                self._create_rectangle(
+                    width=213, height=480, color=(255, 0, 0)
+                ),  # blue
+                self._create_rectangle(
+                    width=214, height=480, color=(255, 255, 255)
+                ),  # white
+                self._create_rectangle(width=213, height=480, color=(0, 0, 255)),  # red
+            ]
+        )
+
+        # shrink and center it
+        M = np.float32([[0.5, 0, width / 4], [0, 0.5, height / 4]])
+        data_bgr = cv2.warpAffine(data_bgr, M, (width, height))
+
+        # compute animation
+        omega = 2 * np.pi / height
+        id_x = np.tile(np.array(range(width), dtype=np.float32), (height, 1))
+        id_y = np.tile(
+            np.array(range(height), dtype=np.float32), (width, 1)
+        ).transpose()
+
+        self.frames = []
+        for k in range(30):
+            phase = 2 * k * np.pi / 30
+            map_x = id_x + 10 * np.cos(omega * id_x + phase)
+            map_y = id_y + 10 * np.sin(omega * id_x + phase)
+            self.frames.append(
+                VideoFrame.from_ndarray(
+                    cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR), format="bgr24"
+                )
+            )
+            
+    async def recv(self):
+        pts, time_base = await self.next_timestamp()
+
+        frame = self.frames[self.counter % 30]
+        frame.pts = pts
+        frame.time_base = time_base
+        self.counter += 1
+        return frame
+
+    def _create_rectangle(self, width, height, color):
+        data_bgr = np.zeros((height, width, 3), np.uint8)
+        data_bgr[:, :] = color
+        return data_bgr
 
 
+async def run(pc, recorder, signaling, role):
+    def add_tracks():
+        pc.addTrack(FlagVideoStreamTrack())
+
+    @pc.on("track")
+    def on_track(track):
+        print("Receiving %s" % track.kind)
+        recorder.addTrack(track)
+
+    # connect signaling
+    await signaling.connect()
+
+    if role == "offer":
+        # send offer
+        add_tracks()
+        await pc.setLocalDescription(await pc.createOffer())
+        await signaling.send(pc.localDescription)
+
+    # consume signaling
+    while True:
+        obj = await signaling.receive()
+
+        if isinstance(obj, RTCSessionDescription):
+            await pc.setRemoteDescription(obj)
+            await recorder.start()
+
+            if obj.type == "offer":
+                # send answer
+                add_tracks()
+                await pc.setLocalDescription(await pc.createAnswer())
+                await signaling.send(pc.localDescription)
+        elif isinstance(obj, RTCIceCandidate):
+            await pc.addIceCandidate(obj)
+        elif obj is BYE:
+            print("Exiting")
+            break
 
 
 
@@ -20,8 +139,33 @@ if __name__ == '__main__':
     print("Starting Server")
     
     # create signaling object
-    signaling = create_signaling()
+    signaling = TcpSocketSignaling(host = "0.0.0.0", port = "8080")
     
     
     # create peer connection
     pc = RTCPeerConnection()
+    
+    # create recorder
+    recorder = MediaBlackhole()
+    
+    loop = asyncio.get_event_loop()
+    
+    try:
+        loop.run_until_complete(
+            run(
+                pc=pc,
+                recorder=recorder,
+                signaling=signaling,
+                role='offer'
+                )
+            )
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # cleanup
+        loop.run_until_complete(recorder.stop())
+        loop.run_until_complete(signaling.close())
+        loop.run_until_complete(pc.close())
+        
+    
+    
