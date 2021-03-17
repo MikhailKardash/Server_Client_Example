76479c30fbec127b2fee61e066c29d70154a57ed
MikhailKardash <48943383+MikhailKardash@users.noreply.github.com>
2021-03-16 17:27:32 -0700

    Update server.py
    Added docstrings and some comments.

--- server.py
+++ server.py
@@ -17,17 +17,22 @@ from aiortc.contrib.signaling import BYE, add_signaling_arguments, TcpSocketSign
 
 class CircleVideoStreamTrack(VideoStreamTrack):
     """
-    A video track that returns an animated flag.
+    A video track that returns a moving circle.
+    I hardcode this to have height, width of 480x640
+    Circle has a radius of 50.
+    Animation lasts 60 frames, 30 moving in one direction
+    and 30 moving in the opposite direction.
+    Construction is done in numpy.
     """
 
     def __init__(self):
         super().__init__()  # don't forget this!
-        self.counter = 0
+        self.counter = 0 #frame state.
         height, width = 480, 640
         radius = 50
 
         # generate circle
-        data_bgr = self._create_circle(radius, (255,0,255)).astype(np.float32)
+        data_bgr = self._create_circle(radius, (255,255,255)).astype(np.float32)
         
         # generate background
         background = np.zeros((height, width, 3))
@@ -55,6 +60,9 @@ class CircleVideoStreamTrack(VideoStreamTrack):
             )
             
     async def recv(self):
+        """
+        Async function that advances the frame state.
+        """
         pts, time_base = await self.next_timestamp()
 
         frame = self.frames[self.counter % 60]
@@ -64,6 +72,10 @@ class CircleVideoStreamTrack(VideoStreamTrack):
         return frame
         
     def _create_circle(self, radius, color):
+        """
+        Helper function that draws a circle in RGB/BGR.
+        Circle has diameter of 2*radius + 1.
+        """
         data_bgr = np.zeros((radius*2+1, radius*2+1, 3), np.uint8)
         cx, cy = radius + 1, radius + 1
         y, x = np.ogrid[-radius:radius,-radius:radius]
@@ -73,36 +85,59 @@ class CircleVideoStreamTrack(VideoStreamTrack):
 
 
 def channel_send(channel, message):
+    """
+    Send message over specified channel.
+    """
     channel.send(message)
 
-async def run_offer(pc, signaling, window_name):
+async def run_offer(pc, signaling):
+    """
+    Function that handles all signaling.
+    Creates chat channel and track channel.
+    Constantly pings clients for responses.
+    Sends video stream to client.
+    Leaves connection open to client, waiting
+    for responses.
+    """
+    
+    #create channel and reference stream.
     channel = pc.createDataChannel("chat")
     reference = CircleVideoStreamTrack()
-    frame = await reference.recv()
     
     def add_tracks():
+        """
+        function that adds video stream track
+        to signal.
+        """
         pc.addTrack(CircleVideoStreamTrack())
 
     await signaling.connect()
     
-    async def get_video():
-        while True:
-            frame = await track.recv()
-            frame = frame.to_ndarray()
-            cv2.imshow(window,frame)
-            cv2.waitKey(1)
-    
     async def send_pings():
+        """
+        function that constantly sends pings
+        through chat channel.
+        """
         while True:
             channel_send(channel, "ping")
             await asyncio.sleep(1)
 
     @channel.on("open")
     def on_open():
+        """
+        Function that makes sure that pings keep
+        being sent.
+        """
         asyncio.ensure_future(send_pings())
 
     @channel.on("message")
     def on_message(message):
+        """
+        Function that processes feedback from client.
+        If feedback is pong, do nothing.
+        If feedback is coordinates, process current frame
+        and then calculate error.
+        """
         if isinstance(message,str):
             print(message)
             if message == 'pong':
@@ -121,6 +156,8 @@ async def run_offer(pc, signaling, window_name):
     add_tracks()
     await pc.setLocalDescription(await pc.createOffer())
     await signaling.send(pc.localDescription)
+    
+    # keep connection open.
     while True:
         frame = await reference.recv()
         obj = await signaling.receive()
@@ -142,6 +179,11 @@ async def run_offer(pc, signaling, window_name):
             break
 
 def calc_coords(frame):
+    """
+    Calculate coordinates.
+    Make sure that it is consistent with client side
+    processing method.
+    """
     frame = frame[10:480,10:640]
     for y in range(frame.shape[0]):
         for x in range(frame.shape[1]):
@@ -159,9 +201,6 @@ if __name__ == '__main__':
     
     # create peer connection
     pc = RTCPeerConnection()
-    
-    # video window name
-    window_name = 'ServerStream'
     
     # create event and run loop
     event = run_offer(pc,signaling, window_name)
